dcd26e7ab3fdc42dcb628ce90b18c566766898a1
CHARLES GIROUARD <charlesgirouard2@gmail.com>
2023-02-11 15:06:49 +0100

    changing colors and footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="flex col center padding1 footer ">
-      <p>Fait par Terry Grimoire</p>
-      <Link to="/Mentions">
-        <p className="small">Mentions Légales</p>
-      </Link>
+    <footer className="flex-col center padding1 footer ">
+      <p>Suivez-nous sur les réseaux :</p>
+      <div className="flex">
+        <Link to="/Mentions">
+          <p className="small">Mentions Légales</p>
+        </Link>
+        <Link to="/Mentions">
+          <p className="small">Politique de confidentialité</p>
+        </Link>
+      </div>
+      <p>Site développé par le Grimoire Numérique. Design par Jayce Salez.</p>
+      <p>Réalisé avec le soutien de la Région Réunion</p>
     </footer>
   );
 }
